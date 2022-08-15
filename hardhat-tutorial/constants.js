41f98ccd905c670bc3fddd67b681b0553eb60427
Shubham Sharma <shubhamku044@gmail.com>
2022-08-15 14:46:15 +0530

    completed the dapp

--- hardhat-tutorial/constants.js
+++ hardhat-tutorial/constants.js
@@ -1,4 +1,4 @@
 const WHITELIST_CONTRACT_ADDRESS = '0xaE2298d1670C36f2855f0C434c23C2e305fD1d4e'
-const METADATA_URL = ''
+const METADATA_URL = 'https://nft-collection-one.vercel.app/api/'
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL }
